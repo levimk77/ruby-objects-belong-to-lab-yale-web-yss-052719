f3c83d9f8f7bbbbf42fd93ca39854ae78475ef87
Elijah Florence <efloren@siue.edu>
2019-05-29 19:27:32 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,11 @@
 class Post 
+  attr_accessor :title, :name, :author
+  
   def initialize(title, name, author)
     @title = title
     @name = name
     @author = author
   end
   
-  attr_accessor :title, :name, :author
+ 
 end 
