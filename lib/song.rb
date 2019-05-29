fdcb415e1f722f1c5725b7c6934535300e795307
Elijah Florence <efloren@siue.edu>
2019-05-29 19:30:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,13 @@
 class Song 
+  attr_accessor :title, :artist
+  
   @@allSongs = []
+  
   def initialize
     @title = title
     @artist = artist
     @allSongs << self
   end
   
-  attr_accessor :title, :artist
+
 end 
